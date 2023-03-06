3eb1cfab33cfb21d8858e525966db1319077f62d
Jonathan Henrique Alves de Lima <38362824+JonathanHenriqueCWB@users.noreply.github.com>
2023-03-06 20:11:25 -0300

    Funcionamento do ReactDOM

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,8 @@
+// ReactDOM
+import ReactDOM from 'react-dom'
+
+// Pegando id da div da página principal da SPA (public/index.html)
+const conteudo = document.getElementById('root')
+
+// Usando o react dom para renderizar conteúdo
+ReactDOM.render('Olá mundo', conteudo)
