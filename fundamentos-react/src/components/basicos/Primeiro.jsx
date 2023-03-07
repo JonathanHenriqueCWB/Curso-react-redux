0750d8e62b9cddf7017162d424075364add23d73
Jonathan Henrique Alves de Lima <38362824+JonathanHenriqueCWB@users.noreply.github.com>
2023-03-07 16:27:44 -0300

    Criando um componente

---
+++ fundamentos-react/src/components/basicos/Primeiro.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Primeiro() {
+
+    const mensagem = 'Primeiro componente criado em react!'
+
+    return (
+        <div>
+            <h1>Olá mundo</h1>
+            <p> {mensagem} </p>
+        </div>
+    )
+}
+
+export default Primeiro
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,15 @@
 import ReactDOM from 'react-dom'
 // Import Reacto para utilização do JSX
 import React from 'react'
+// Import CSS
+import './index.css'
+// Primeiro compnente
+import Primeiro from './components/basicos/Primeiro'
 
 // Usando o react dom para renderizar conteúdo
 // É obrigatório a importação do React para utilzação do JSX
 // JSX Também pode ser atribuido a uma variável
 ReactDOM.render(
-    <div>Olá mundo!!!</div>,
+    <Primeiro />,
     document.getElementById('root')
 )
